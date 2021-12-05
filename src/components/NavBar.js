730cd27f9a81d539177ba87ac14d0464cb65ddfb
Gabriela Gaiba <gabrielagaiba@gmail.com>
2021-12-05 12:55:04 -0300

    Desafio 2: menu e-commerce

---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark nav-bar">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#"><img src="/logo-rabona.png" className="logo" alt="logo" /></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">     
+            <li className="nav-item">
+              <a className="nav-link" href="#">Tienda</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Locales</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Franquicias</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Nosotros</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+  
+export default NavBar;
